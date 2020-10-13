e4190c10a2072b59e025f6ff17c648bfed208c7e
Emma <69841693+xentraz@users.noreply.github.com>
2020-10-13 10:13:43 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
 
+// const trigger_search = document.querySelector(".trigger_search");
+// const input = document.querySelector(".input");
+
+// trigger_search.addEventListener("click", () => {
+//   if (!input.classList.contains("input-open")) {
+//     input.classList.add("input-open");
+//     trigger_search.innerHTML = "  <i class='fas fa-times'>  </i>";
+//   } else {
+//     input.classList.remove("input-open");
+//     trigger_search.innerHTML = "  <i class='fas fa-search'>  </i>";
+//   }
+// });
